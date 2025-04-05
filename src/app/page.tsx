880ee4db92c6d98eb198dca67aa36c6fd52b555a
Felipe Rossetto <feliperossettodasilva123@gmail.com>
2025-04-05 18:24:16 -0300

    chore: add Tailwind CSS setup and shadcn/UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main>
-      <div>Hello world!</div>
+      <div>
+        <Button>Enviar</Button>
+      </div>
     </main>
   );
 }
